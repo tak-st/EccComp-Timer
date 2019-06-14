d0a7204d2f8dcb0a711ba2b668b573e5bdd8459b
Takuya Shintani <2180264@ecccomp.com>
2019-06-14 12:06:19 +0900

    日付はやはりスペースないほうが様々なフォントに対応しやすい。

--- Timer/My Project/AssemblyInfo.vb
+++ Timer/My Project/AssemblyInfo.vb
@@ -33,5 +33,5 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("1.0.1.0")>
-<Assembly: AssemblyFileVersion("2.1.1")>
+<Assembly: AssemblyFileVersion("2.1.2")>
 <Assembly: NeutralResourcesLanguage("ja-JP")>
